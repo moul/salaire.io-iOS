f2bf6f4641f08c16c7619790c5aa4569bfd60980
Manfred Touron <m@42.am>
2016-06-03 16:04:22 +0200

    Add text inputs

--- Salaire/ViewController.swift
+++ Salaire/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var HourlyGrossValue: UITextField!
+    @IBOutlet weak var HourlyNetValue: UITextField!
+    @IBOutlet weak var MonthlyNetValue: UITextField!
+    @IBOutlet weak var MonthlyGrossValue: UITextField!
+    @IBOutlet weak var YearlyNetValue: UITextField!
+    @IBOutlet weak var YearlyGrossValue: UITextField!
+    
+    @IBAction func HourlyGrossChanged(sender: AnyObject) {
+        print(HourlyGrossValue.text)
+    }
+    
+    @IBAction func HourlyNetChanged(sender: AnyObject) {
+        print(HourlyNetValue.text)
+    }
+    
+    @IBAction func MonthlyGrossChanged(sender: AnyObject) {
+        print(MonthlyGrossValue.text)
+    }
+    
+    @IBAction func MonthlyNetChanged(sender: AnyObject) {
+        print(MonthlyNetValue.text)
+    }
+    
+    @IBAction func YearlyGrossChanged(sender: AnyObject) {
+        print(YearlyGrossValue.text)
+    }
+    
+    @IBAction func YearlyNetChanged(sender: AnyObject) {
+        print(YearlyNetValue.text)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
